41197477dfd35fd9cd2d7d3fe3c071dce6c19a44
VidurWadhwa <vidur2wadhwa@gmail.com>
2017-07-26 23:15:45 +0530

    Minor changes

--- app/src/main/java/vidur/codeclan/bridge/Utils/UniversalImageLoader.java
+++ app/src/main/java/vidur/codeclan/bridge/Utils/UniversalImageLoader.java
@@ -1,12 +1,19 @@
 package vidur.codeclan.bridge.Utils;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import com.nostra13.universalimageloader.cache.memory.impl.WeakMemoryCache;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.FailReason;
 import com.nostra13.universalimageloader.core.assist.ImageScaleType;
 import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
 
 import vidur.codeclan.bridge.R;
 
@@ -39,5 +46,50 @@ public class UniversalImageLoader {
                 .diskCacheSize(100 * 1024 * 1024).build();
 
         return configuration;
+    }
+
+//    Can only used set if the images are static and are not setup for
+//    a list view or grid view
+
+    public void setImage(String ImageUrl, ImageView iv_put, final ProgressBar mProgressBar, String append) {
+        //append is to get the various types of files like from the web or drawables
+        //https://github.com/VidurWadhwa/Android-Universal-Image-Loader
+
+        ImageLoader imageLoader = ImageLoader.getInstance();
+
+        imageLoader.displayImage(append + ImageUrl, iv_put, new ImageLoadingListener() {
+            @Override
+            public void onLoadingStarted(String imageUri, View view) {
+
+                if(mProgressBar != null){
+                    mProgressBar.setVisibility(View.VISIBLE);
+                }
+
+            }
+
+            @Override
+            public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+                if(mProgressBar != null){
+                    mProgressBar.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                if(mProgressBar != null){
+                    mProgressBar.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onLoadingCancelled(String imageUri, View view) {
+                if(mProgressBar != null){
+                    mProgressBar.setVisibility(View.GONE);
+                }
+            }
+        });
+
+
+
     }
 }
